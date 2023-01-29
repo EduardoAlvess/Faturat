93cbc4187e1d1843a17333b09ee58425c49d85c9
Eduardo Alves <eduardo.silva@umbler.com>
2023-01-29 16:31:12 -0300

    add, edit and delete account

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -24,37 +24,60 @@ namespace TCC.Controllers
             return View(list);
         }
 
-        [HttpPost]
-        public IActionResult Create([FromBody] Account account)
+        [HttpGet]
+        public ActionResult Add()
         {
+            var account = new Account();
+
+            return PartialView("_AddAccountModal", account);
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, double balance)
+        {
+            Account account = new Account()
+            {
+                Name = name,
+                Balance = balance,
+                isDeleted = false,
+                CreationDate = DateTime.Now,
+                UserId = _userProvider.GetUserId()
+            };
+
             _databaseContext.Accounts.Add(account);
             _databaseContext.SaveChanges(account, "Added");
-            return Json("Teste");
+
+            return Redirect("/Accounts");
         }
 
-
-        [HttpPost]
-        public ActionResult Edit([FromBody] Account account)
-        {
-            var accountToEdit = _databaseContext.Accounts.FirstOrDefault(x => x.Id == account.Id && x.UserId == _userProvider.GetUserId());
-
-            if(account != null)
-            {
-                accountToEdit.Balance = account.Balance;
-                accountToEdit.Name = account.Name;
-                _databaseContext.SaveChanges(accountToEdit, "Modified");
-                return Json("Atualizado");
-            }
-            return Json("Erro");
-        }
 
         [HttpGet]
-        public ActionResult Delete(int id)
+        public ActionResult Edit(int id)
+        {
+            var account = _databaseContext.Accounts.FirstOrDefault(x => x.Id == id && x.UserId == _userProvider.GetUserId());
+
+            return PartialView("_EditAccountModal", account);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, string name, double balance)
+        {
+            var accountToEdit = _databaseContext.Accounts.FirstOrDefault(x => x.Id == id && x.UserId == _userProvider.GetUserId());
+
+            accountToEdit.Name = name;
+            accountToEdit.Balance = balance;
+
+            _databaseContext.SaveChanges(accountToEdit, "Modified");
+
+            return Redirect("/Accounts");
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
         {
             var account = _databaseContext.Accounts.FirstOrDefault(x => x.Id == id && x.UserId == _userProvider.GetUserId());
             account.isDeleted = true;
             _databaseContext.SaveChanges(account, "Modified");
-            return Json("Teste");
         }
 
         [HttpPost]
